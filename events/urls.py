48c1761bcdb7860c042c54bf03e3fc98ae5a2a62
easin <easin562050@gmail.com>
2025-04-29 07:21:06 +0600

    event urls fixed

--- events/urls.py
+++ events/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
-from events.views import event_page,dashboard,create_event,create_participant,create_category,search,update_event,delete_event,update_participant,delete_participant
+from events.views import home_page,event_page,dashboard,base,create_event,create_participant,create_category,search,update_event,delete_event,update_participant,delete_participant,user_dashboard,rsvp_event
 urlpatterns = [
-  path('', search, name="home"),
-  # path('home-page/',home_page,name="home-page"),
+  path('home-page/',home_page,name="home-page"),
   path('event-page/<int:id>/',event_page,name="event-page"),
   path('create-event/',create_event,name="create-event"),
   path('create-participant/',create_participant,name="create-participant"),
   path('create-category/',create_category,name="create-category"),
   path('dashboard/',dashboard,name="dashboard"),
   path('search/',search,name="search"),
-  # path('base/',base,name="base"),
+  path('base/',base,name="base"),
+  path('event/<int:event_id>/rsvp/', rsvp_event, name='rsvp_event'),
+  path('user-dashboard/', user_dashboard, name="user_dashboard"),
   path('update/<int:id>/',update_event,name="update"),
   path('delete/<int:id>/',delete_event,name="delete"),
   path('update_participant/<int:id>/',update_participant,name="update_participant"),
