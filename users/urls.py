98ab5c63200919ec2d633a53d570da0dbbae4e5a
easin <easin562050@gmail.com>
2025-05-28 21:32:25 +0600

    profile design done

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from users.views import UserProfileView,sign_up, SignIn, sign_out, activate_user, AdminDashboard, assign_role, create_group, group_list
-
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('sign-up/', sign_up, name='sign-up'),
     path('sign-in/', SignIn.as_view(), name='sign-in'),
-    path('sign-out/', sign_out, name='logout'),
+    path('sign-out/', LogoutView.as_view(), name='logout'),
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('activate/<int:user_id>/<str:token>/', activate_user),
     path('admin/admin-dashboard/', AdminDashboard.as_view(), name='admin-dashboard'),
