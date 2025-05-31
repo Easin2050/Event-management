2291815f277ab349f670a128842e35c5015b2f25
easin <easin562050@gmail.com>
2025-05-31 11:13:55 +0600

    error fixing

--- event_management/settings.py
+++ event_management/settings.py
@@ -111,7 +111,10 @@ DATABASES = {
         'USER': config('DB_USER', default=''),
         'PASSWORD': config('DB_PASSWORD', default=''),
         'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', cast=int)
+        'PORT': config('DB_PORT', cast=int),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 # Password validation
@@ -122,7 +125,7 @@ DATABASES = {
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://event_management_db_lyeh_user:DQXQFeuaGByz6xCroph4scFrwkCEaAgo@dpg-d0s76549c44c73crrmmg-a.oregon-postgres.render.com/event_management_db_lyeh',
+        default='postgresql://event_management_db_tb3a_user:xQxgYoLhwgyQeM9HSdLcWQFK3K8w05h6@dpg-d0s89hp5pdvs739an120-a.oregon-postgres.render.com/event_management_db_tb3a',
         conn_max_age=600
     )
 }
