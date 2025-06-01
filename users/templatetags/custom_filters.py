bc6c9176fdc2e2b166b3ef2cd5c442027eb9b155
easin <easin562050@gmail.com>
2025-06-01 20:10:08 +0600

    template_tags are done

--- users/templatetags/custom_filters.py
+++ users/templatetags/custom_filters.py
@@ -1,18 +1,19 @@
-from django import template
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.utils import timezone
+from django import template
 
 register = template.Library()
-
 
 @register.filter
 def humanized_date(value):
     if value:
-        today = datetime.now().date()
         value = timezone.localtime(value)
+        today = timezone.localtime(timezone.now()).date()
+        yesterday = today - timedelta(days=1)
+
         if value.date() == today:
             return f"Today at {value.strftime('%I:%M %p')}"
-        if value.date() == today.replace(day=today.day - 1):
+        elif value.date() == yesterday:
             return f"Yesterday at {value.strftime('%I:%M %p')}"
         else:
             return f"{value.date().strftime('%B %d')}, {value.strftime('%I:%M %p')}"
